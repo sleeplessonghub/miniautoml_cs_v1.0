f4137ba602384836c4e267c8a0cc3a9cf4117f87
sleeplessonghub <ra4.sleepless@gmail.com>
2026-01-14 16:17:40 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -626,13 +626,13 @@ if st.session_state['df_pp'] is not None:
             pdp = best_model_explainer.model_profile(random_state = 42, verbose = False)
             pdp_fig: go.Figure = pdp.plot(show = False, y_title = "") # 'y_title' was a bitch to find (hours!!!), had to dig through the dev's source code
             pdp_fig_ss = st.session_state['pdp_fig_ss'] = pdp_fig.update_layout(showlegend = False,
-                                                                                height = 600 if len(feature_train.columns) >= 3 else None,
+                                                                                height = 600 if len(feature_train.columns) >= 3 else 400 if len(feature_train.columns) == 2 else None,
                                                                                 width = None,
                                                                                 autosize = True,
                                                                                 title_x = 0.5,
                                                                                 margin = dict(l = 50),
                                                                                 hovermode = 'closest',
-                                                                                hoverlabel = dict(bgcolor = '#8dc5cc')).update_traces(hovertemplate = '- X-Axis Value: <b>%{x:.4f}</b>' + '<br>- Avg. Target Z-Score: <b>%{y:.4f}</b><extra></extra>')
+                                                                                hoverlabel = dict(bgcolor = '#8dc5cc')).update_traces(hovertemplate = '✦ X-Axis Value: <b>%{x:.4f}</b>' + '<br>✦ Avg. Target Z-Score: <b>%{y:.4f}</b><extra></extra>')
             st.plotly_chart(pdp_fig_ss, width = 'stretch', config = {'displayModeBar': False})
           
           elif is_object == True: # Classification modeling
