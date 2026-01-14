418ce460ba8347f060e2cefd56908b220272218d
sleeplessonghub <ra4.sleepless@gmail.com>
2026-01-14 14:04:55 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -625,7 +625,8 @@ if st.session_state['df_pp'] is not None:
             st.write('• Partial Dependence Plots (PDPs):')
             pdp = best_model_explainer.model_profile(random_state = 42, verbose = False)
             pdp_fig: go.Figure = pdp.plot(show = False)
-            pdp_fig.update_yaxes(title_text = None)
+            pdp_fig.update_yaxes(title_text = "")
+            pdp_fig.update_layout(yaxis_title = "")
             pdp_fig_ss = st.session_state['pdp_fig_ss'] = pdp_fig.update_layout(showlegend = False,
                                                                                 height = 600,
                                                                                 width = None,
