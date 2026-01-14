97fc720382b07a9db996c5e490fe00af467b8500
sleeplessonghub <ra4.sleepless@gmail.com>
2026-01-14 08:48:09 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -610,8 +610,8 @@ if st.session_state['df_pp'] is not None:
             ).strip())
 
             st.write('• Permutation Feature Importance (PFI):')
-            best_model_explainer_ss = st.session_state['best_model_explainer_ss'] = best_model_explainer.model_parts(random_state = 42).plot()
-            st.plotly_chart(best_model_explainer_ss)
+            best_model_explainer_ss = st.session_state['best_model_explainer_ss'] = best_model_explainer.model_parts(random_state = 42)
+            best_model_explainer_ss
 
             st.write('• Partial Dependence Plots (PDPs):')
             pdp = best_model_explainer.model_profile(random_state = 42, verbose = False)
