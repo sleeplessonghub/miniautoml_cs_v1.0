18edaefb477bc47dff51793c11c31c4d321f6d14
sleeplessonghub <ra4.sleepless@gmail.com>
2026-01-14 11:35:06 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -615,7 +615,7 @@ if st.session_state['df_pp'] is not None:
             st.write('• Permutation Feature Importance (PFI):')
             pfi = best_model_explainer.model_parts(random_state = 42)
             pfi_fig: go.Figure = pfi.plot(show = False)
-            pfi_fig_ss = st.session_state['pfi_fig_ss'] = pfi_fig.update_layout(height = 300,
+            pfi_fig_ss = st.session_state['pfi_fig_ss'] = pfi_fig.update_layout(height = 300 if len(feature_train.columns) >= 6 else 250,
                                                                                 width = None,
                                                                                 autosize = True,
                                                                                 title_font_size = 17,
