37d81174567f79aa06e96a577b8e2b1135a63d9f
sleeplessonghub <ra4.sleepless@gmail.com>
2026-01-29 11:18:16 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -784,8 +784,10 @@ if st.session_state['df_pp'] is not None:
                   tabs = st.tabs(interpretation_tabs_list)
                   for i, tab in enumerate(tabs):
                     tab.dataframe(interpretation_tables_list[i].map(lambda x: str(int(float(x))) if (str(x).replace('.', '', 1).isdigit() and str(x).endswith('.0')) else (str(round(x, 4)) if isinstance(x, float) else str(x))),
-                                  386 if len(target_encoded_vars) > 10 else 'auto',
-                                  column_config = {'Category': st.column_config.Column(width = 200)})
+                                  300 if len(target_encoded_vars) > 10 else 'auto',
+                                  column_config = {'Category': st.column_config.Column(width = 200),
+                                                   'min': st.column_config.Column(width = 200),
+                                                   'max': st.column_config.Column(width = 200)})
 
               st.session_state['data_tracker_check'] = st.session_state['data_tracker'] # Data tracker check update
 
@@ -832,8 +834,10 @@ if st.session_state['df_pp'] is not None:
                   tabs = st.tabs(interpretation_tabs_list)
                   for i, tab in enumerate(tabs):
                     tab.dataframe(interpretation_tables_list[i].map(lambda x: str(int(float(x))) if (str(x).replace('.', '', 1).isdigit() and str(x).endswith('.0')) else (str(round(x, 4)) if isinstance(x, float) else str(x))),
-                                  386 if len(target_encoded_vars) > 10 else 'auto',
-                                  column_config = {'Category': st.column_config.Column(width = 200)})
+                                  300 if len(target_encoded_vars) > 10 else 'auto',
+                                  column_config = {'Category': st.column_config.Column(width = 200),
+                                                   'min': st.column_config.Column(width = 200),
+                                                   'max': st.column_config.Column(width = 200)})
           
           elif is_object == True: # Classification modeling
 
@@ -1042,8 +1046,10 @@ if st.session_state['df_pp'] is not None:
                   tabs = st.tabs(interpretation_tabs_list)
                   for i, tab in enumerate(tabs):
                     tab.dataframe(interpretation_tables_list[i].map(lambda x: str(int(float(x))) if (str(x).replace('.', '', 1).isdigit() and str(x).endswith('.0')) else (str(round(x, 4)) if isinstance(x, float) else str(x))),
-                                  386 if len(target_encoded_vars) > 10 else 'auto',
-                                  column_config = {'Category': st.column_config.Column(width = 200)})
+                                  300 if len(target_encoded_vars) > 10 else 'auto',
+                                  column_config = {'Category': st.column_config.Column(width = 200),
+                                                   'min': st.column_config.Column(width = 200),
+                                                   'max': st.column_config.Column(width = 200)})
               
               st.session_state['data_tracker_check'] = st.session_state['data_tracker'] # Data tracker check update
             
@@ -1090,8 +1096,10 @@ if st.session_state['df_pp'] is not None:
                   tabs = st.tabs(interpretation_tabs_list)
                   for i, tab in enumerate(tabs):
                     tab.dataframe(interpretation_tables_list[i].map(lambda x: str(int(float(x))) if (str(x).replace('.', '', 1).isdigit() and str(x).endswith('.0')) else (str(round(x, 4)) if isinstance(x, float) else str(x))),
-                                  386 if len(target_encoded_vars) > 10 else 'auto',
-                                  column_config = {'Category': st.column_config.Column(width = 200)})
+                                  300 if len(target_encoded_vars) > 10 else 'auto',
+                                  column_config = {'Category': st.column_config.Column(width = 200),
+                                                   'min': st.column_config.Column(width = 200),
+                                                   'max': st.column_config.Column(width = 200)})
 
           # E
 
